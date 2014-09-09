619732ad149de42cce744a526593ffde57ab409f
Duco Visbeen <duco@maximum.com>
2014-09-09 11:40:55 +0200

    string fix

--- app/views/admin/version/create.blade.php
+++ app/views/admin/version/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 
-@section('title', HTML::linkAction('PluginController@index','Plugins') . ' > ' . HTML::linkAction('PluginController@show',$plugin->name,[$plugin->id]) . ' > add version')
+@section('title', HTML::linkAction('PluginController@index','Plugins') . ' > ' . HTML::linkAction('PluginController@show',$plugin->name,[$plugin->id]) . ' > new version')
 
 @section('page')
 <p>test</p>
